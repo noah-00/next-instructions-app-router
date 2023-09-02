5915088af23ae2e8aaffc5c11374dcc0fdd81e2a
noah-00 <t71.ishizaka.naoya@gmail.com>
2023-09-02 14:51:11 +0900

    ✅ Defining Routes

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+// this component under the app directory is segment
+// Just follow these steps to create a page! (URL/dashbord)
+
+import React from "react";
+
+function page() {
+  return <div>dashboard page</div>;
+}
+
+export default page;
---
+++ src/app/dashboard/settings/page.tsx
@@ -0,0 +1,7 @@
+// this page is Leaf segment (URL/dashboard/settings)
+
+import React from "react";
+
+export default function setting() {
+  return <div>setting</div>;
+}
