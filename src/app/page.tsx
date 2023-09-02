bbbb484e89e4c4f590cc3e903c0e74cb72b77aa4
noah-00 <t71.ishizaka.naoya@gmail.com>
2023-09-03 08:33:30 +0900

    ✨ add title or meta

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 // layout.tsx and page.tsx file. These will be rendered when the user visits the root of your application (/).
 
+// In the app directory, you can modify the <head> HTML elements such as title and meta using the built-in SEO support.
+import { Metadata } from "next";
+export const metadata: Metadata = {
+  title: "next-instructions-app-router",
+};
+
 export default function Page() {
   return <h1>Hello, Next.js!</h1>;
 }
