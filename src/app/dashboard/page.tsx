f8e55ec4800458c19232dd5a12232bf5f90cf00f
noah-00 <t71.ishizaka.naoya@gmail.com>
2023-09-04 08:25:33 +0900

    ✨ linking and navigating

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,30 @@
+"use client";
+
 // this component under the app directory is segment
 // Just follow these steps to create a page! (URL/dashbord)
 
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 import React from "react";
 
-function page() {
-  return <div>dashboard page</div>;
+function Page() {
+  const router = useRouter();
+  const handleClick = () => {
+    router.push("/dashboard");
+  };
+
+  return (
+    <div>
+      <h1>dashboard page</h1>
+      <Link href="/dashboard" className="bg-blue-500 inline-block p-1 mr-1">
+        to /dashboard
+      </Link>
+      {/* or */}
+      <button onClick={handleClick} className="bg-blue-500 inline-block p-1">
+        to /dashboard
+      </button>
+    </div>
+  );
 }
 
-export default page;
+export default Page;
